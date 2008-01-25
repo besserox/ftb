3706ec04631b8e4b6c449df17bc2be7a9eed5891
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2008-01-25 19:28:36 +0000

    adding ftb_auxil.h
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/trunk@373 b452418e-dd38-0410-89fe-f8a55b138f88

---
+++ src/include/ftb_auxil.h
@@ -0,0 +1,31 @@
+/*
+ * This file contains some auxillary functions useful for FTB
+ * The functions in this file are independent of FTB
+ */
+
+#ifndef FTB_AUXIL_H
+#define FTB_AUXIL_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+/*Remove the spaces from the start and end of string*/
+char * trim_string (const char *str);
+
+//Ensure that the last parameter in the below function call is NULL
+//Ensure that str1 is the destination string, large enough to hold the
+//concatentation of all strings
+int concatenate_strings (char *str1, ...);
+
+/*Check it the string is composed of alphanumeric & underscore characters
+ * only 
+ */
+int check_alphanumeric_underscore_format(const char *str);
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
