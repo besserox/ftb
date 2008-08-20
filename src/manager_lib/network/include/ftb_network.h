141998e9407e3007ba4fa7bc1eda866bc382629f
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2008-08-20 18:43:35 +0000

    Formatted ftb_network.h
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/trunk@487 b452418e-dd38-0410-89fe-f8a55b138f88

--- src/manager_lib/network/include/ftb_network.h
+++ src/manager_lib/network/include/ftb_network.h
@@ -4,41 +4,48 @@
 #include "ftb_def.h"
 #include "ftb_manager_lib.h"
 
-/*Generic information common for all network, used for authentication and topology management*/
+/* 
+ * Generic information common for all network, used for authentication
+ * and topology management 
+ */
 typedef struct FTBN_config_info {
     uint32_t FTB_system_id;
     int leaf;
-}FTBN_config_info_t;
+} FTBN_config_info_t;
 
 #if defined(FTB_NETWORK_SOCK)
 #include "ftb_network_sock.h"
 #elif defined(FTB_NETWORK_SOME_OTHER_NETWORK)
-/*FILLME: some other header file for other networks*/
+/* FILLME: some other header file for other networks */
 #endif
 
 #ifdef __cplusplus
 extern "C" {
-#endif 
+#endif
 
 #if defined(FTB_NETWORK_SOCK)
 typedef FTBN_addr_sock_t FTBN_addr_t;
 #elif defined(FTB_NETWORK_SOME_OTHER_NETWORK)
-/*FILLME: some other header file for other networks*/
+/* FILLME: some other header file for other networks */
 #endif
 
-int FTBN_Init(const FTB_location_id_t *my_id, const FTBN_config_info_t *config_info);
+int FTBN_Init(const FTB_location_id_t * my_id, const FTBN_config_info_t * config_info);
 
-int FTBN_Connect(const FTBM_msg_t *reg_msg, FTB_location_id_t *parent_location_id);
+int FTBN_Connect(const FTBM_msg_t * reg_msg, FTB_location_id_t * parent_location_id);
 
 int FTBN_Finalize(void);
 
-int FTBN_Send_msg(const FTBM_msg_t *msg);
+int FTBN_Send_msg(const FTBM_msg_t * msg);
 
-/*If blocking == 0, it returns FTBN_NO_MSG indicating no new message, or FTB_SUCCESS indicating new message received*/
+/* 
+ * If blocking == 0, it returns FTBN_NO_MSG indicating no new message,
+ * or FTB_SUCCESS indicating new message received
+ */
 #define FTBN_NO_MSG               0x1
-int FTBN_Recv_msg(FTBM_msg_t *msg, FTB_location_id_t *incoming_src,  int blocking);
 
-int FTBN_Disconnect_peer(const FTB_location_id_t *peer_location_id);
+int FTBN_Recv_msg(FTBM_msg_t * msg, FTB_location_id_t * incoming_src, int blocking);
+
+int FTBN_Disconnect_peer(const FTB_location_id_t * peer_location_id);
 
 int FTBN_Get_my_network_address(char *addr);
 
@@ -46,6 +53,6 @@ int FTBN_Abort(void);
 
 #ifdef __cplusplus
 } /*extern "C"*/
-#endif 
+#endif
 
 #endif /* FTB_NETWORK_H */
