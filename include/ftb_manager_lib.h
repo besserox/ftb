a65d55f82dc9d367c5adcf7c0622499d79fb3b30
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2008-08-20 19:22:52 +0000

    Formatted ftb_manager_lib.h
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/trunk@500 b452418e-dd38-0410-89fe-f8a55b138f88

--- include/ftb_manager_lib.h
+++ include/ftb_manager_lib.h
@@ -25,7 +25,7 @@ typedef struct FTBM_msg {
     FTB_id_t src;
     FTB_id_t dst;
     FTB_event_t event;
-}FTBM_msg_t;
+} FTBM_msg_t;
 
 int FTBM_Init(int leaf);
 
@@ -33,41 +33,37 @@ int FTBM_Finalize(void);
 
 int FTBM_Abort(void);
 
-int FTBM_Get_location_id(FTB_location_id_t *location_id);
+int FTBM_Get_location_id(FTB_location_id_t * location_id);
 
-int FTBM_Get_parent_location_id(FTB_location_id_t *location_id);
+int FTBM_Get_parent_location_id(FTB_location_id_t * location_id);
 
-int FTBM_Client_register(const FTB_id_t *id);
-   
-int FTBM_Client_deregister(const FTB_id_t *id);
+int FTBM_Client_register(const FTB_id_t * id);
 
-/*
-Communication calls
-*/
-int FTBM_Send(const FTBM_msg_t *msg);
+int FTBM_Client_deregister(const FTB_id_t * id);
+
+/* Communication calls */
+int FTBM_Send(const FTBM_msg_t * msg);
 
 #define FTBM_NO_MSG           0x1
-int FTBM_Poll(FTBM_msg_t *msg, FTB_location_id_t *incoming_src);
+int FTBM_Poll(FTBM_msg_t * msg, FTB_location_id_t * incoming_src);
 
-int FTBM_Wait(FTBM_msg_t *msg, FTB_location_id_t *incoming_src);
+int FTBM_Wait(FTBM_msg_t * msg, FTB_location_id_t * incoming_src);
 
-/*
-Local book-keeping
-*/
-int FTBM_Register_subscription(const FTB_id_t *id, FTB_event_t *event);
+/* Local book-keeping */
+int FTBM_Register_subscription(const FTB_id_t * id, FTB_event_t * event);
 
-int FTBM_Cancel_subscription(const FTB_id_t *id, FTB_event_t *event);
+int FTBM_Cancel_subscription(const FTB_id_t * id, FTB_event_t * event);
 
-int FTBM_Register_publishable_event(const FTB_id_t *id, FTB_event_t *event);
+int FTBM_Register_publishable_event(const FTB_id_t * id, FTB_event_t * event);
 
-int FTBM_Publishable_event_registration_cancel(const FTB_id_t *id, FTB_event_t *event);
+int FTBM_Publishable_event_registration_cancel(const FTB_id_t * id, FTB_event_t * event);
 
-int FTBM_Get_catcher_comp_list(const FTB_event_t *event, FTB_id_t **list, int *len);
+int FTBM_Get_catcher_comp_list(const FTB_event_t * event, FTB_id_t ** list, int *len);
 
-int FTBM_Release_comp_list(FTB_id_t *list);
+int FTBM_Release_comp_list(FTB_id_t * list);
 
 #ifdef __cplusplus
 } /*extern "C"*/
 #endif
 
-#endif /*FTB_MANAGER_H*/
+#endif /*FTB_MANAGER_H */
