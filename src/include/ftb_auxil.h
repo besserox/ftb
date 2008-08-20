177c9dc9ebb170d92f3aec52530e6248e69dd312
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2008-08-20 18:54:01 +0000

    Formatted ftb_auxil.h and ftb_util.h
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/trunk@489 b452418e-dd38-0410-89fe-f8a55b138f88

--- src/include/ftb_auxil.h
+++ src/include/ftb_auxil.h
@@ -6,24 +6,27 @@
 #ifndef FTB_AUXIL_H
 #define FTB_AUXIL_H
 
-
 #ifdef __cplusplus
 extern "C" {
-#endif 
+#endif
 
-/*Remove the spaces from the start and end of string*/
-char * trim_string (const char *str);
+
+/* Remove the spaces from the start and end of string */
+char *trim_string(const char *str);
 
 
 void soft_trim(char **str);
 
-//Ensure that the last parameter in the below function call is NULL
-//Ensure that str1 is the destination string, large enough to hold the
-//concatentation of all strings
-int concatenate_strings (char *str1, ...);
+/* Ensure that the last parameter in the below function call is NULL.
+ * Also Ensure that str1 is the destination string, large enough to hold the
+ * concatentation of all strings
+ */
+int concatenate_strings(char *str1, ...);
 
-/*Check it the string is composed of alphanumeric & underscore characters
- * only 
+
+/*
+ * Check it the string is composed of alphanumeric & underscore characters
+ * only
  */
 int check_alphanumeric_underscore_format(const char *str);
 
