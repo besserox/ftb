54114ea10fb79d3864acca8965e6544c9c9ebdf0
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2008-08-20 02:19:51 +0000

    Cleaned up ftb_auxil.c
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/trunk@480 b452418e-dd38-0410-89fe-f8a55b138f88

--- src/util/ftb_auxil.c
+++ src/util/ftb_auxil.c
@@ -6,57 +6,61 @@
 
 void soft_trim(char **str)
 {
-  int i=0,j=0;
-  int length = strlen(*str);
-  char *e;
+    int i = 0, j = 0;
+    int length = strlen(*str);
+    char *e;
 
-  if ( **str == '\0' ) return;
+    if (**str == '\0')
+	return;
 
-  e = *str + length - 1;
+    e = *str + length - 1;
 
-  while ( *(*str+i) == ' ' || *(*str+i) == '\t' ) i++;
-  *str = *str + i;
-  j = length - i;
+    while (*(*str + i) == ' ' || *(*str + i) == '\t')
+	i++;
+    *str = *str + i;
+    j = length - i;
 
-  while (j>0 &&  (*e == ' ' || *e == '\t')  ) {
-    j--; e--;
-  }
+    while (j > 0 && (*e == ' ' || *e == '\t')) {
+	j--;
+	e--;
+    }
 
-  if ( j < length - i ) *(e+1) = '\0';
+    if (j < length - i)
+	*(e + 1) = '\0';
 }
 
-char * trim_string (const char *str) 
+char *trim_string(const char *str)
 {
-    int i=0,j=0;
-    char *new_str = (char*)malloc(strlen(str)+1);
-    strcpy(new_str,str);
-    while((new_str[i]==' ')||(new_str[i]=='\t')) 
-        i++; 
-    if(i>0) {
-        for(j=0;j<strlen(new_str);j++) 
-            new_str[j]=new_str[j+i];
-        new_str[j]='\0';
-    }    
-    i=strlen(new_str)-1;
-    while((new_str[i]==' ')||(new_str[i]=='\t')) 
-        i--; 
-    if(i<(strlen(new_str)-1)) 
-        new_str[i+1]='\0';
+    int i = 0, j = 0;
+    char *new_str = (char *) malloc(strlen(str) + 1);
+    strcpy(new_str, str);
+    while ((new_str[i] == ' ') || (new_str[i] == '\t'))
+	i++;
+    if (i > 0) {
+	for (j = 0; j < strlen(new_str); j++)
+	    new_str[j] = new_str[j + i];
+	new_str[j] = '\0';
+    }
+    i = strlen(new_str) - 1;
+    while ((new_str[i] == ' ') || (new_str[i] == '\t'))
+	i--;
+    if (i < (strlen(new_str) - 1))
+	new_str[i + 1] = '\0';
     return new_str;
 }
 
 int concatenate_strings(char *str, ...)
 {
-    char * tempstr;
+    char *tempstr;
 
     va_list arguments;
     va_start(arguments, str);
 
     tempstr = va_arg(arguments, char *);
-    strcpy(str,tempstr);
+    strcpy(str, tempstr);
 
-    while ((tempstr = va_arg(arguments, char *)) != NULL){
-      strcat(str, tempstr);
+    while ((tempstr = va_arg(arguments, char *)) != NULL) {
+	strcat(str, tempstr);
     }
 
     va_end(arguments);
@@ -65,12 +69,15 @@ int concatenate_strings(char *str, ...)
 
 int check_alphanumeric_underscore_format(const char *str)
 {
-    int i=0; 
-    for (i=0; i<strlen(str); i++) {
-        if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] == '_') || (str[i]>='0' && str[i]<='9')) 
-            continue;
-        else
-            return 0; 
+    int i = 0;
+    for (i = 0; i < strlen(str); i++) {
+	if ((str[i] >= 'A' && str[i] <= 'Z') 
+		|| (str[i] >= 'a' && str[i] <= 'z') 
+		|| (str[i] == '_') 
+		|| (str[i] >= '0' && str[i] <= '9'))
+	    continue;
+	else
+	    return 0;
     }
-    return 1; // Return value of '1' indicates that string is alphanumeric & underscore compliant
+    return 1;	/* Return value of '1' indicates that string is alphanumeric & underscore compliant */
 }
