c181848b8e54c9016f0cffa9a414d81a36257fa3
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2008-08-20 19:07:34 +0000

    Formatted ftb_client_lib.h
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/trunk@493 b452418e-dd38-0410-89fe-f8a55b138f88

--- src/client_lib/include/ftb_client_lib.h
+++ src/client_lib/include/ftb_client_lib.h
@@ -4,34 +4,56 @@
 #include "ftb_manager_lib.h"
 #include "ftb_def.h"
 
-int FTBC_Connect(const FTB_client_t *cinfo, uint8_t extension, FTB_client_handle_t *client_handle);
+int FTBC_Connect(const FTB_client_t * cinfo, uint8_t extension, FTB_client_handle_t * client_handle);
 
-int FTBC_Declare_publishable_events(FTB_client_handle_t client_handle, const char * schema_file, const FTB_event_info_t  *einfo, int num_events);
 
-int FTBC_Publish(FTB_client_handle_t client_handle, const char *event_name, const FTB_event_properties_t *event_properties, FTB_event_handle_t *event_handle);
+int FTBC_Declare_publishable_events(FTB_client_handle_t client_handle, const char *schema_file,
+				    const FTB_event_info_t * einfo, int num_events);
 
-int FTBC_Subscribe_with_polling(FTB_subscribe_handle_t *subscribe_handle, FTB_client_handle_t client_handle, const char *subscription_str);
 
-int FTBC_Subscribe_with_callback(FTB_subscribe_handle_t *subscribe_handle, FTB_client_handle_t handle, const char *subscription_str, int (*callback)(FTB_receive_event_t *, void*), const void *arg);
+int FTBC_Publish(FTB_client_handle_t client_handle, const char *event_name,
+		 const FTB_event_properties_t * event_properties, FTB_event_handle_t * event_handle);
 
-int FTBC_Unsubscribe(FTB_subscribe_handle_t *subscribe_handle);
 
-int FTBC_Poll_event(FTB_subscribe_handle_t subscribe_handle, FTB_receive_event_t *receive_event);
+int FTBC_Subscribe_with_polling(FTB_subscribe_handle_t * subscribe_handle,
+				FTB_client_handle_t client_handle, const char *subscription_str);
+
+
+int FTBC_Subscribe_with_callback(FTB_subscribe_handle_t * subscribe_handle, FTB_client_handle_t handle,
+				 const char *subscription_str, int (*callback) (FTB_receive_event_t *,
+										void *),
+				 const void *arg);
+
+
+int FTBC_Unsubscribe(FTB_subscribe_handle_t * subscribe_handle);
+
+
+int FTBC_Poll_event(FTB_subscribe_handle_t subscribe_handle, FTB_receive_event_t * receive_event);
+
 
 int FTBC_Reg_throw(FTB_client_handle_t handle, const char *event);
 
+
 int FTBC_Disconnect(FTB_client_handle_t handle);
 
-int FTBC_Get_event_handle(const FTB_receive_event_t receive_event, FTB_event_handle_t *event_handle);
 
-int FTBC_Compare_event_handles(const FTB_event_handle_t event_handle1, const FTB_event_handle_t event_handle2);
+int FTBC_Get_event_handle(const FTB_receive_event_t receive_event, FTB_event_handle_t * event_handle);
+
+
+int FTBC_Compare_event_handles(const FTB_event_handle_t event_handle1,
+			       const FTB_event_handle_t event_handle2);
+
 
 #ifdef FTB_TAG
-int FTBC_Add_dynamic_tag(FTB_client_handle_t handle, FTB_tag_t tag, const char *tag_data, FTB_tag_len_t data_len);
+int FTBC_Add_dynamic_tag(FTB_client_handle_t handle, FTB_tag_t tag, const char *tag_data,
+			 FTB_tag_len_t data_len);
+
 
 int FTBC_Remove_dynamic_tag(FTB_client_handle_t handle, FTB_tag_t tag);
 
-int FTBC_Read_dynamic_tag(const FTB_receive_event_t *event, FTB_tag_t tag, char *tag_data, FTB_tag_len_t *data_len);
+
+int FTBC_Read_dynamic_tag(const FTB_receive_event_t * event, FTB_tag_t tag, char *tag_data,
+			  FTB_tag_len_t * data_len);
 #endif
 
 #endif
