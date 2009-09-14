94efb2d809aa6e32caccac61f29370874cd532d0
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2009-09-14 01:16:07 +0000

    removing unnecessary version information from each file
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/trunk@850 b452418e-dd38-0410-89fe-f8a55b138f88

--- components/autogen.sh
+++ components/autogen.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 #*****************************************************************************
-# FTB:ftb-info
-#  This software is licensed under BSD. See the file FTB/misc/license.BSD for
-#  complete details on your rights to copy, modify, and use this software.
-# FTB:ftb-info
+#  This file is part of FTB (Fault Tolerance Backplance) - the core of CIFTS
+#  (Co-ordinated Infrastructure for Fault Tolerant Systems)
+#  
+#  See http://www.mcs.anl.gov/research/cifts for more information.
+#  	
 
-# FTB:ftb-fillin
-#  FTB_Version: 0.6.1
-#  FTB_API_Version: 0.5
-#  FTB_Heredity:FOSS_ORIG
-#  FTB_License:BSD
-# FTB:ftb-fillin
-# FTB:ftb-bsd
 #  This software is licensed under BSD. See the file FTB/misc/license.BSD for
 #  complete details on your rights to copy, modify, and use this software.
-# FTB:ftb-bsd
 #*****************************************************************************
 
 usage()
