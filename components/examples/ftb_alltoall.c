b7f4bd0271eaaa1f49350f2bd5962259d00378dd
rgupta <rgupta@b452418e-dd38-0410-89fe-f8a55b138f88>
2009-09-14 00:39:50 +0000

    Tagging 0.6.1 release
    git-svn-id: https://svn.mcs.anl.gov/repos/cifts/ftb/tags/releases/ftb-0.6.1@845 b452418e-dd38-0410-89fe-f8a55b138f88

--- components/examples/ftb_alltoall.c
+++ components/examples/ftb_alltoall.c
@@ -1,21 +1,18 @@
 /**********************************************************************************/
-/* FTB:ftb-info */
-/* This software is licensed under BSD. See the file FTB/misc/license.BSD for
- * complete details on your rights to copy, modify, and use this software.
+/* This file is part of FTB (Fault Tolerance Backplance) - the core of CIFTS
+ * (Co-ordinated Infrastructure for Fault Tolerant Systems)
+ * 
+ * See http://www.mcs.anl.gov/research/cifts for more information.
+ * 	
  */
-/* FTB:ftb-info */
-/* FTB:ftb-fillin */
 /* FTB_Version: 0.6.1
  * FTB_API_Version: 0.5
  * FTB_Heredity:FOSS_ORIG
  * FTB_License:BSD
  */
-/* FTB:ftb-fillin */
-/* FTB:ftb-bsd */
 /* This software is licensed under BSD. See the file FTB/misc/license.BSD for
  * complete details on your rights to copy, modify, and use this software.
  */
-/* FTB:ftb-bsd */
 /*********************************************************************************/
 
 /* A sample MPI application.
